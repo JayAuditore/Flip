d00b0a47738692bd13415435f432e570308a5419
heiyukauidou <80405916+heiyukauidou@users.noreply.github.com>
2021-03-26 16:30:35 +0800

    Update script name
    更新一下类的命名

--- Flip/Assets/Scripts/Interacting with the scene/Interactingwithobjects.cs
+++ Flip/Assets/Scripts/Interacting with the scene/Interactingwithobjects.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Interactingwithobjects : MonoBehaviour
+public class InteractingWithObjects : MonoBehaviour
 {
     
     public TMP_Text text;
