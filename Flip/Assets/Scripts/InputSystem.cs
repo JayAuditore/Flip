9bb6aa15f419c153d08d539d60e538560967b9a9
Jay_Auditore <993574833@qq.com>
2021-03-28 15:34:45 +0800

    Add PlayerController
    添加了人物移动，例如：走、跑、跳、下蹲

---
+++ Flip/Assets/Scripts/InputSystem.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Setting/InputSystem.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputSystem : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputSystem()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputSystem"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""0bb87b3b-9d7e-4268-9d4e-6d47ab723d20"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""91a594eb-8f87-41d3-9ab3-bb7b84660b94"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""58ce9dad-50a8-46be-91f4-b7b49e90cf7c"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""HorizontalMove"",
+                    ""id"": ""68115be0-d94d-4632-8fb4-c73bc12a30bf"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""f291fb7d-a0f0-4e22-8a56-46dab49b68d8"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""d5cdf7d1-6e89-4296-83dc-d3c0bdc1dbba"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""675f116b-0afe-4f04-904b-594903637314"",
+                    ""path"": ""<Keyboard>/leftCtrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""555ae777-a0a4-49ac-8659-5746898b1990"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Move = m_GamePlay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Move;
+    public struct GamePlayActions
+    {
+        private @InputSystem m_Wrapper;
+        public GamePlayActions(@InputSystem wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_GamePlay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    public interface IGamePlayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Flip/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region 字段
+
+    [SerializeField] private bool jumpPressed;
+    [SerializeField] private bool crouchPressed;
+    [SerializeField] private float accelerationTime;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float speed;
+    [SerializeField] private float augmentedVelocity;
+    private int jumpCount;
+    private Rigidbody2D rb;
+
+    public bool IsGrounded;
+    public bool IsJumping;
+    public bool IsAccelerating;
+    public Transform GroundCheck;
+    public Transform CeilingCheck;
+    public BoxCollider2D Coll;
+    public LayerMask Ground;
+
+    #endregion
+
+    #region Unity回调
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && IsGrounded)
+        {
+            IsAccelerating = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            IsAccelerating = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouchPressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouchPressed = false;
+            Coll.size = new Vector2(Coll.size.x, Coll.size.y * 2);
+            Coll.offset = new Vector2(Coll.offset.x, Coll.offset.y + (Coll.size.y / 4));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, Ground);
+        GroundMove();
+        Jump();
+        Crouch();
+    }
+
+    #endregion
+
+    #region 方法
+
+    //移动
+    void GroundMove()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalMove != 0 && IsAccelerating)
+        {
+            rb.velocity = new Vector2(Mathf.Lerp(horizontalMove * speed, horizontalMove * augmentedVelocity, Time.deltaTime * (1 / accelerationTime)), rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+        }
+
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+    }
+
+    //跳跃
+    void Jump()
+    {
+        if (IsGrounded)
+        {
+            jumpCount = 1;
+            IsJumping = false;
+        }
+        if (jumpPressed && IsGrounded)
+        {
+            IsJumping = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+
+    //蹲下
+    void Crouch()
+    {
+        if (crouchPressed && IsGrounded)
+        {
+            Coll.size = new Vector2(Coll.size.x, Coll.size.y / 2);
+            Coll.offset = new Vector2(Coll.offset.x, Coll.offset.y - (Coll.size.y / 2));
+            crouchPressed = false;
+        }
+    }
+
+    #endregion
+}
