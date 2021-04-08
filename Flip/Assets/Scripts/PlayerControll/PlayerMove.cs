b0c69e1d8ebd540b6957fff50f45f29abeb081a0
Jay_Auditore <993574833@qq.com>
2021-04-08 21:58:53 +0800

    Update: Leap, run and crouch
    完善了跳跃和移动

--- Flip/Assets/Scripts/PlayerControll/PlayerMove.cs
+++ Flip/Assets/Scripts/PlayerControll/PlayerMove.cs
@@ -9,17 +9,26 @@ namespace Flip.PlayerControll
     {
         #region 字段
 
+        private float accelerationTimer;
+        private float slowDownTimer;
+        private float crouchTimer;
+        private Vector2 Velocity;
         private Rigidbody2D rb;
         private PlayerInput playerInput;
 
+        [Header("Float")]
         public float AccelerationTime;
         public float JumpForce;
+        public float RushJumpForce;
         public float Speed;
         public float CrouchSpeed;
         public float AugmentedVelocity;
+        [Space]
+        [Header("Component")]
         public Transform GroundCheck;
         public Transform CeilingCheck;
         public BoxCollider2D Coll;
+        [Space]
         public LayerMask Ground;
 
         #endregion
@@ -34,75 +43,123 @@ namespace Flip.PlayerControll
 
         void Update()
         {
-            if (Input.GetKeyUp(KeyCode.S))
-            {
-                Coll.size = new Vector2(Coll.size.x, Coll.size.y * 2);
-                Coll.offset = new Vector2(Coll.offset.x, Coll.offset.y + (Coll.size.y / 4));
-            }
+            playerInput.IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, Ground);
+            playerInput.CanJump = !Physics2D.OverlapCircle(CeilingCheck.position, 0.1f, Ground);
+            Crouch();
+            Jump();
         }
 
         void FixedUpdate()
         {
-            playerInput.IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.1f, Ground);
-            playerInput.CanJump = Physics2D.OverlapCircle(CeilingCheck.position, 0.1f, Ground);
-            Move();
+            GroundMove();
         }
 
         #endregion
 
         #region 方法
 
-        //移动
-        void GroundMove()
+        //计算速度
+        public Vector2 CalculateVelocity()
         {
             float horizontalMove = Input.GetAxisRaw("Horizontal");
 
+            //有左右输入
             if (horizontalMove != 0)
             {
+                slowDownTimer = 0;
+
                 //地面上
                 if (playerInput.IsGrounded)
                 {
                     //冲刺
                     if (playerInput.IsAccelerating)
                     {
-                        rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, AugmentedVelocity * horizontalMove, Time.deltaTime * (1 / 0.1f)), rb.velocity.y);
+                        accelerationTimer += Time.fixedDeltaTime * (1 / AccelerationTime);
+                        Velocity = new Vector2(Mathf.Lerp(rb.velocity.x, AugmentedVelocity * horizontalMove, accelerationTimer), rb.velocity.y);
                     }
                     //蹲下
                     else if (playerInput.CrouchPressed)
                     {
-                        rb.velocity = new Vector2(CrouchSpeed * horizontalMove, rb.velocity.y);
+                        crouchTimer += Time.fixedDeltaTime * (1 / 0.2f);
+                        Velocity = new Vector2(Mathf.Lerp(rb.velocity.x, horizontalMove * CrouchSpeed, crouchTimer), rb.velocity.y);
                     }
                     //正常走
                     else
                     {
-                        rb.velocity = new Vector2(Speed * horizontalMove, rb.velocity.y);
+                        Velocity = new Vector2(horizontalMove * Speed, rb.velocity.y);
+                    }
+
+                    //重置timer
+                    if (!playerInput.IsAccelerating)
+                    {
+                        accelerationTimer = 0;
+                    }
+                    if (!playerInput.CrouchPressed)
+                    {
+                        crouchTimer = 0;
                     }
                 }
                 //空中
                 else if (!playerInput.IsGrounded)
                 {
-                    rb.velocity = new Vector2(Speed * horizontalMove, rb.velocity.y);
+                    //同向移动
+                    if (rb.velocity.x * horizontalMove > 0)
+                    {
+                        Velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+                    }
+                    //反向移动
+                    if (rb.velocity.x * horizontalMove < 0)
+                    {
+                        Velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+                    }
                 }
             }
-            else
+            //无左右输入
+            else if (horizontalMove == 0)
             {
-                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, Time.deltaTime * (1 / 0.2f)), rb.velocity.y);
-
+                slowDownTimer += Time.fixedDeltaTime * (1 / 5f);
+                Velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, slowDownTimer), rb.velocity.y);
             }
+
+            return Velocity;
+        }
+
+        //速度赋值
+        void GroundMove()
+        {
+            rb.velocity = CalculateVelocity();
         }
 
         //跳跃
         void Jump()
         {
-            if (!playerInput.CanJump)
+            if (playerInput.CanJump)
             {
+                //初始化
                 if (playerInput.IsGrounded && !playerInput.CrouchPressed)
                 {
                     playerInput.JumpCount = 1;
                     playerInput.IsJumping = false;
                 }
 
-                if (playerInput.JumpPressed && playerInput.IsGrounded)
+                //普通的跳跃
+                if (playerInput.NormalJumping && playerInput.IsGrounded)
+                {
+                    playerInput.IsJumping = true;
+                    rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+                    playerInput.JumpCount--;
+                    playerInput.JumpPressed = false;
+                }
+                //跑跳
+                else if (playerInput.RushJumping && rb.velocity.x != 0 && playerInput.IsGrounded)
+                {
+                    playerInput.IsJumping = true;
+                    rb.velocity = new Vector2(rb.velocity.x, RushJumpForce);
+                    playerInput.JumpCount--;
+                    playerInput.JumpPressed = false;
+                }
+                //按住shift原地起跳
+                else if (playerInput.RushJumping && playerInput.IsGrounded)
                 {
                     playerInput.IsJumping = true;
                     rb.velocity = new Vector2(rb.velocity.x, JumpForce);
@@ -115,18 +172,20 @@ namespace Flip.PlayerControll
         //蹲下
         void Crouch()
         {
+            //蹲下
             if (Input.GetKeyDown(KeyCode.S))
             {
-                Coll.size = new Vector2(Coll.size.x, Coll.size.y / 2);
-                Coll.offset = new Vector2(Coll.offset.x, Coll.offset.y - (Coll.size.y / 2));
+                Coll.size = new Vector2(Coll.size.x, 0.5f);
+                Coll.offset = new Vector2(Coll.offset.x, -0.25f);
+                playerInput.CanJump = false;
             }
-        }
-
-        void Move()
-        {
-            GroundMove();
-            Jump();
-            Crouch();
+            //站立
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                Coll.size = new Vector2(Coll.size.x, 1f);
+                Coll.offset = new Vector2(Coll.offset.x, 0f);
+                playerInput.CanJump = true;
+            }
         }
 
         #endregion
