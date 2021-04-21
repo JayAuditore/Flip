8fc4ab6a580c65721022c1c3543f8521a58b6f02
Jay_Auditore <993574833@qq.com>
2021-04-21 19:33:14 +0800

    Fix: Interact
    修复了交互代码的格式

--- Flip/Assets/Scripts/GameMechanics/Interact/PushObject.cs
+++ Flip/Assets/Scripts/GameMechanics/Interact/PushObject.cs
@@ -8,14 +8,17 @@ namespace Flip.Interact
     public class PushObject : MonoBehaviour
     {
         #region 字段
+
         // TODO: 增加一个地面检测
         public float Velocity;
         public LayerMask LayerMask;
         public RaycastHit2D[] RaycastHit2D;
 
-
         private EntityInput entityInput;
+
         #endregion
+
+        #region Unity回调
 
         private void Awake()
         {
@@ -26,28 +29,36 @@ namespace Flip.Interact
         {
             Push(this.gameObject, 1.5f, LayerMask);
         }
-        public void Canpush()//检测到之后要做的事情
-        {
 
+        #endregion
+
+        #region 方法
+
+        //检测到之后要做的事情
+        public void Canpush()
+        {
             if (entityInput.IsPushing && ((RaycastHit2D[0].transform.position.x - transform.position.x) * transform.localScale.x > 0))
             {
                 RaycastHit2D[0].transform.position = RaycastHit2D[0].transform.position + new Vector3(Velocity * Time.fixedDeltaTime * transform.localScale.x, 0, 0);
-                
             }
             else
             {
-            }
 
+            }
         }
+
         //target表示检测到的物品，rads代表检测的范围，collidermask表示要检测的图层
         public void Push(GameObject target, float rads, LayerMask collidermask)
         {
             RaycastHit2D = Physics2D.RaycastAll(target.transform.position , new Vector3(1, 0, 0) * transform.localScale.x, rads, LayerMask);
+
             if (RaycastHit2D.Length > 0)
             {
                 Canpush();
             }
         }
+
+        #endregion
     }
 }
 
