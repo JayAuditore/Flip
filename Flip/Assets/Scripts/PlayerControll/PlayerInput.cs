4ff61ce32fc531689ca0c67858f62fb45fa24bff
heiyukauidou <80405916+heiyukauidou@users.noreply.github.com>
2021-04-22 17:38:44 +0800

    Revert "Fix: PushObject"
    This reverts commit 4c6a84fc5581fe547a5926107ef54e92b20be41a.

--- Flip/Assets/Scripts/PlayerControll/PlayerInput.cs
+++ Flip/Assets/Scripts/PlayerControll/PlayerInput.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Flip.Module;
-using Flip.Interact;
 
 namespace Flip.PlayerControll
 {
     public class PlayerInput : BaseSingletonWithMono<PlayerInput>
     {
         private EntityInput entityInput;
-        private PushObject pushObject;
 
         #region Unity回调
 
         void Awake()
         {
             entityInput = GetComponent<EntityInput>();
-            pushObject = GetComponent<PushObject>();
         }
 
         void Update()
@@ -82,7 +79,7 @@ namespace Flip.PlayerControll
             }
 
             //推箱子
-            if (pushObject.IsPushing)
+            if (Input.GetKey(KeyCode.F))
             {
                 entityInput.IsPushing = true;
             }
