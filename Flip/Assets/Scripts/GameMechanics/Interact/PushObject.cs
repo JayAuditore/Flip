4c6a84fc5581fe547a5926107ef54e92b20be41a
Jay_Auditore <993574833@qq.com>
2021-04-22 17:15:29 +0800

    Fix: PushObject
    修改了IsPushing的位置

--- Flip/Assets/Scripts/GameMechanics/Interact/PushObject.cs
+++ Flip/Assets/Scripts/GameMechanics/Interact/PushObject.cs
@@ -10,22 +10,17 @@ namespace Flip.Interact
         #region 字段
 
         // TODO: 增加一个地面检测
+        public bool IsPushing;
         public float Velocity;
         public LayerMask LayerMask;
         public RaycastHit2D[] RaycastHit2D;
 
-        private Rigidbody2D rigidbody2DOfObject;    
-        private EntityInput entityInput;
+        private Rigidbody2D rigidbody2DOfObject;
 
         #endregion
 
         #region Unity回调
 
-        private void Awake()
-        {
-            entityInput = GetComponent<EntityInput>();   
-        }
-        
         public void FixedUpdate()
         {
             Push(this.gameObject, 1.5f, LayerMask);
@@ -38,23 +33,29 @@ namespace Flip.Interact
         //检测到之后要做的事情
         public void Canpush()
         {
-            if (entityInput.IsPushing && ((RaycastHit2D[0].transform.position.x - transform.position.x) * transform.localScale.x > 0))
+            //检测键盘输入
+            if (Input.GetKey(KeyCode.F))
             {
-                rigidbody2DOfObject = RaycastHit2D[0].transform.GetComponent<Rigidbody2D>();
-                rigidbody2DOfObject.mass = 0.1f;
-                //RaycastHit2D[0].transform.position = RaycastHit2D[0].transform.position + new Vector3(Velocity * Time.fixedDeltaTime * transform.localScale.x, 0, 0);
-            }
-            else
-            {
-                rigidbody2DOfObject = RaycastHit2D[0].transform.GetComponent<Rigidbody2D>();
-                rigidbody2DOfObject.mass = 100;
+                IsPushing = true;
+                //下面的我看不懂
+                if ((RaycastHit2D[0].transform.position.x - transform.position.x) * transform.localScale.x > 0)
+                {
+                    rigidbody2DOfObject = RaycastHit2D[0].transform.GetComponent<Rigidbody2D>();
+                    rigidbody2DOfObject.mass = 0.1f;
+                    //RaycastHit2D[0].transform.position = RaycastHit2D[0].transform.position + new Vector3(Velocity * Time.fixedDeltaTime * transform.localScale.x, 0, 0);
+                }
+                else
+                {
+                    rigidbody2DOfObject = RaycastHit2D[0].transform.GetComponent<Rigidbody2D>();
+                    rigidbody2DOfObject.mass = 100;
+                }
             }
         }
 
         //target表示检测到的物品，rads代表检测的范围，collidermask表示要检测的图层
         public void Push(GameObject target, float rads, LayerMask collidermask)
         {
-            RaycastHit2D = Physics2D.RaycastAll(target.transform.position , new Vector3(1, 0, 0) * transform.localScale.x, rads, LayerMask);
+            RaycastHit2D = Physics2D.RaycastAll(target.transform.position, new Vector2(1, 0) * transform.localScale.x, rads, LayerMask);
 
             if (RaycastHit2D.Length > 0)
             {
