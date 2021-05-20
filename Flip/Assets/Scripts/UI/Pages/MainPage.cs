3bf64704dbe3898b21b47d3f324d09e670b15dc1
Jay_Auditore <993574833@qq.com>
2021-05-20 17:56:32 +0800

    Update: Create main menu
    做了主菜单页面的一些基本功能

---
+++ Flip/Assets/Scripts/UI/Pages/MainPage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Flip.UI
+{
+    public class MainPage : BaseUI
+    {
+        #region 登记的页面
+
+        public OptionPage OptionPage;
+        public QuitPage QuitPage;
+        public MainMenu MainMenu;
+
+        #endregion
+
+        #region Unity回调
+
+        private void Update()
+        {
+            if (MainMenu.transform.localRotation.eulerAngles.y > 90f)
+            {
+                MainMenu.transform.Find("Background").gameObject.SetActive(true);
+            }
+            else
+            {
+                MainMenu.transform.Find("Background").gameObject.SetActive(false);
+            }
+        }
+
+        #endregion
+
+        #region 方法
+
+        //点击选项按钮
+        public void OptionPageOnClick()
+        {
+            MainMenu.transform.DORotate(new Vector3(0f, 180f, 0f), 3f);
+        }
+
+        //点击退出按钮
+        public void QuitPageOnClick()
+        {
+            QuitPage.Show();
+        }
+
+        //在选项界面点返回
+        public void OptionPageBackOnClick()
+        {
+            MainMenu.transform.DORotate(new Vector3(0f, 0f, 0f), 3f);
+        }
+
+        #endregion
+    }
+}
